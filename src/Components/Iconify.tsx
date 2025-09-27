a04e886250129b03df37074752d9b15abda868b6
Phillip Coitchev <p.coitchev@devrealm.bg>
2025-09-27 13:10:51 +0300

    Add Iconify

---
+++ src/Components/Iconify.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import { Icon } from '@iconify/react';
+import { IconifyIcon } from '@iconify/react';
+import Box, { BoxProps } from '@mui/material/Box';
+
+interface IProps extends BoxProps {
+  icon?: IconifyIcon | string;
+}
+
+// flaticon.com
+// https://icon-sets.iconify.design/?category=General
+
+
+
+const Iconify = forwardRef<SVGElement, IProps>(({ icon, width = 20, sx, ...other }, ref) => (
+  <Box
+    ref={ref}
+    component={Icon}
+    className="component-iconify"
+    icon={icon}
+    sx={{ width, height: width, ...sx }}
+    {...other}
+  />
+));
+
+
+
+export default Iconify;
